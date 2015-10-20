ae9a6cc8e139f1fc5910d1c45afa0a74acc07585
Cley Faye <github@cleyfaye.net>
2015-10-20 20:16:18 +0200

    Added CLI args handling into imager.py
     - Added command-line arguments to imager.py

--- imager.py
+++ imager.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 from fosfile import Vault
 from PIL import Image, ImageDraw
 
@@ -21,7 +22,7 @@ def enlargeRect(rect, point):
         rect[3] = point[1]
 
 def main(config):
-    vault = Vault('data/Vault1_New.sav')
+    vault = Vault(config['input'])
     rect = [0, 0, 0, 0]
     for room in vault.vault.rooms:
         pos = getCoordinates(room.col, room.row, room.getRoomWidth(), config)
@@ -39,16 +40,20 @@ def main(config):
     for rock in vault.vault.rocks:
         pos = getCoordinates(rock.c, rock.r, 2, config)
         drawer.ellipse(pos, outline = 'white', fill='gray')
-    img.save('output.png', 'PNG')
+    img.save(config['output'], 'PNG')
+
+def parseCli():
+    parser = argparse.ArgumentParser(description = 'Produce a picture showing the rooms layout')
+    parser.add_argument('--input', type=argparse.FileType('rb'), required=True, help='Path to the vault file')
+    parser.add_argument('--output', type=argparse.FileType('wb'), default='output.png', help='Path for the output PNG file')
+    parser.add_argument('--roomWidth', type=int, default=30, help='Width of an elevator, in pixel (=1/3 or a room)')
+    parser.add_argument('--roomHeight', type=int, default=60, help='Height of a room, in pixel')
+    parser.add_argument('--roomSpaceX', type=int, default=3, help='Horizontal spacing between rooms')
+    parser.add_argument('--roomSpaceY', type=int, default=3, help='Vertical spacing between rooms')
+    parser.add_argument('--roomOffsetX', type=int, default=0, help='X Offset to start putting the rooms on the output')
+    parser.add_argument('--roomOffsetY', type=int, default=0, help='Y Offset to start putting the rooms on the output')
+    return vars(parser.parse_args())
 
 if __name__ == '__main__':
-    config = {
-            'roomWidth': 30,
-            'roomHeight': 60,
-            'roomSpaceX': 3,
-            'roomSpaceY': 3,
-            'roomOffsetX': 0,
-            'roomOffsetY': 0
-            }
-    main(config)
+    main(parseCli())
 
